ca11757eab4e2a36da430ec3550d6bceae565ca3
jason <jason@qq.com>
2023-04-05 21:21:38 +0800

    bug修复

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-  </div>
-</template>
-<script setup lang="ts">
-  onMounted(()=>{
-    window.location.href="https://2121b.app/nav/?upline=a_ff0019&yttttt=5096.4566.9140";
-  })
-</script>
